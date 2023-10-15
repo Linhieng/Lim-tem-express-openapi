248fb2bc47f06ec114d0a136a8f8c34387be205d
linhieng <790728740@qq.com>
2023-10-15 20:18:40 +0800

    refactor: 修改全局错误处理模块

--- src/interfaces/response.ts
+++ src/interfaces/response.ts
@@ -1,6 +1,23 @@
 import { DoctorInfo } from './dbTable'
 
 /**
+ * 通用的响应体，所有响应体都继承自该接口
+ */
+export interface ResponseGeneral {
+    code: number
+    message: string
+    data?: any
+}
+
+/**
+ * 返回一个失败的请求时的统一接口。
+ * 失败的请求包括 500, 404, 403 等
+ */
+export interface ResponseError extends ResponseGeneral {
+    data?: Array<any>
+}
+
+/**
  * 医生登录响应体
  */
 export interface DoctorLoginResponse {
